0455a41c99c120f5c3667a9838577a9d886414a4
Bruno Oliveira <bruno@abstractj.org>
2014-12-02 18:13:39 -0200

    Initial progress with Keycloak. But still not properly working

--- examples/app.js
+++ examples/app.js
@@ -5,9 +5,9 @@ var express = require('express')
 
 passport.use(new KeycloakStrategy({
        realm: 'goose-realm',
-       host:       'keycloak.org',
-       clientID:     'testid',
-       clientSecret: 'testsecret',
+       host:       '10.0.1.9:8080',
+       clientID:     'goose-passport-js',
+       clientSecret: '09a0d375-5930-4b9c-bd55-e5c02c6e9609',
        callbackURL:  '/callback'
        },
       function(accessToken, idToken, profile, done) {
@@ -27,6 +27,15 @@ app.set(function() {
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
+
+app.get('/callback',
+  passport.authenticate('Keycloak', { failureRedirect: '/url-if-something-fails' }),
+  function(req, res) {
+    if (!req.user) {
+      throw new Error('user null');
+    }
+    res.redirect("/user");
+  });
 
 app.listen(3000);
 console.log("Starting the server");
